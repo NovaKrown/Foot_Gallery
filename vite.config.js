8a909080c58882297c1527fdf7140390828126bd
NickKumar <Timerbob@gmail.com>
2024-11-07 18:41:20 -0700

    get the page started

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// import restart from "vite-plugin-restart";
+// import glsl from "vite-plugin-glsl";
+
+export default {
+  root: "src/",
+  publicDir: "../static/",
+  base: "./",
+  server: {
+    host: true, // Open to local network and display URL
+    open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "../docs", // Output in the docs/ folder
+    emptyOutDir: true, // Empty the folder first
+    sourcemap: true, // Add sourcemap
+  },
+  // plugins: [
+  //   restart({ restart: ["../static/**"] }), // Restart server on static file change
+  //   glsl(), // Handle shader files
+  // ],
+};
